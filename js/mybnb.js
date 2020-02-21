b35e0054ce0cfd05a9ec8e4112dfc67aaf5f6152
Youssef Ben Abdallah <youssefba1997@gmail.com>
2020-02-21 15:12:32 +0100

    aaa

--- js/mybnb.js
+++ js/mybnb.js
@@ -1,4 +1,18 @@
+var img = "";
 
+function openFile(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function () {
+        img = reader.result;
+        console.log(img);
+
+    };
+    reader.readAsDataURL(input.files[0]);
+
+}
+//Read Image from input
+// <input id="image" type="file" name="myFile"class="form-control" onchange="openFile(event)"></input>
 
 // When the user clicks anywhere outside of the modal, close it ??????
 window.onclick = function(event) {
@@ -138,11 +152,15 @@ let createAvatar = () => {
         var putAvatar = document.getElementById("userAvatar")
         var htmlConnected =  `
         <div > 
+        <center>
         <a href="profile1.html">${userNotConnected.firstName} ${userNotConnected.lastName}</a>
-        <button type="submit" onclick="logout()">logout</button>
-       <a href="profile1.html"> <img src="images/featured-img/img-05.jpg" alt="Avatar" class="avatar"></a>
-        </div>
-        `
+        <br>
+         <a href="profile1.html"> <img src="images/featured-img/img-05.jpg" alt="Avatar" class="avatar"></a>
+         <br>
+         <button type="submit" onclick="logout()">Logout</button>
+         </center>
+         </div>
+         `
     putAvatar.innerHTML=htmlConnected ;
 console.log("userconncted");
     
@@ -159,4 +177,104 @@ console.log("user offline");
 }
     
 
+}
+
+
+
+let createProfile = () => {
+    var userNotConnected = JSON.parse(localStorage.getItem("con_user")) || "";
+    // console.log(userNotConnected);
+    
+    if (userNotConnected != "") {
+        var createProfile = document.querySelector(".userName")
+        var htmlProfile =  `
+        <img src="images/featured-img/img-05.jpg" style="height: 100px;width: 100px;">
+        <h5 class="mb-3">User Profile</h5>
+        <div class="row">
+            <div class="col-md-6">
+                <h6>About</h6>
+                <p>
+                    Web Designer, UI/UX Engineer
+                </p>
+                <h6>Hobbies</h6>
+                <p>
+                    Indie music, skiing and hiking. I love the great outdoors.
+                </p>
+            </div>
+            <div class="col-md-6">
+                
+            <div class="col-md-12">
+                <h5 class="mt-2"><span class="fa fa-clock-o ion-clock float-right"></span> Recent Activity</h5>
+                <table class="table table-sm table-hover table-striped">
+                    <tbody>                                    
+                        <tr>
+                            <td>
+                                <strong>Abby</strong> joined ACME Project Team in <strong>`Collaboration`</strong>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Gary</strong> deleted My Board1 in <strong>`Discussions`</strong>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Kensington</strong> deleted MyBoard3 in <strong>`Discussions`</strong>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>John</strong> deleted My Board1 in <strong>`Discussions`</strong>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Skell</strong> deleted his post Look at Why this is.. in <strong>`Discussions`</strong>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!--/row-->
+    </div>
+    <div class="tab-pane" id="messages">
+        <div class="alert alert-dark alert-dismissable">
+            <a class="panel-close close" data-dismiss="alert">×</a> This is an <strong>.alert</strong>. Use this to show important messages to the user.
+        </div>
+        <table class="table table-hover table-striped">
+            <tbody>                                    
+                <tr>
+                    <td>
+                       <span class="float-right font-weight-bold">3 hrs ago</span> Here is your a link to the latest summary report from the..
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                       <span class="float-right font-weight-bold">Yesterday</span> There has been a request on your account since that was..
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                       <span class="float-right font-weight-bold">9/10</span> Porttitor vitae ultrices quis, dapibus id dolor. Morbi venenatis lacinia rhoncus. 
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                       <span class="float-right font-weight-bold">9/4</span> Vestibulum tincidunt ullamcorper eros eget luctus. 
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                       <span class="float-right font-weight-bold">9/4</span> Maxamillion ais the fix for tibulum tincidunt ullamcorper eros. 
+                    </td>
+                </tr>
+            </tbody> 
+        </table>
+    </div>
+         `
+    createProfile.innerHTML=htmlProfile ;
+console.log("userconncted");
+    
+    }
 }
